0ad8d98a7beb3d11523d463208a3a9d81971f01b
Erik P. Hansen <eph@erikphansen.com>
2017-07-16 20:14:58 -0700

    Version 0.1 more or less done
    - Added ability to paste either MLID (with `enter`) or slug (with `option-enter`)
    - Added a help action at the bottom of the menu
    - Added some merchants for testing on testing.womply.com

--- MLIDs.lbaction/Contents/Scripts/default.js
+++ MLIDs.lbaction/Contents/Scripts/default.js
@@ -1,26 +1,66 @@
-// LaunchBar Action Script
-
-var pasteit = function pasteit(arg) {
-  LaunchBar.paste(`${arg}`);
-}
-
 var merchants = [
-  {id: '193', name: 'the ugly mug'},
-  {id: '250', name: 'irish channel'},
-  {id: '398', name: 'mancini de paris'},
-  {id: '197330', name: 'dante pizzeria'},
-  {id: '355259', name: 'euphoria hair'},
+  {name: 'LOCAL'},
+  {name: 'Mancini De Paris', mlid: '398', slug: 'mancini-de-paris'},
+  {name: 'The Ugly Mug', mlid: '193', slug: 'the-ugly-mug'},
+  {name: 'Irish Channel', mlid: '250', slug: 'irish-channel'},
+  {name: 'Dante Pizzeria', mlid: '197330', slug: 'dante-pizzeria'},
+  {name: 'Euphoria Hair', mlid: '355259', slug: 'euphoria-hair'},
+  {name: 'PROD TESTING'},
+  {name: 'Mykonos Restaurant', mlid: '1528303', slug: 'mykonos-restaurant'},
+  {name: 'New Frontier Restaurant', mlid: '1428000', slug: 'new-frontier-restaurant'},
+  {name: 'Sandspike', mlid: '1437524', slug: 'sandspike'},
+  {name: 'Vapor Connect', mlid: '1511211', slug: 'vapor-connect'}
 ]
 
-var actionItems = merchants.map((merchant) => {
-  return {
-    title: `${merchant.name} - ${merchant.id}`,
-    action: 'pasteit',
-    actionArgument: merchant.id,
-    actionRunsInBackground: true // LaunchBar will crash if `LaunchBar.paste()` is not run in the background
+/**
+ * If this action is run with the option key pressed, paste the merchant slug,
+ * otherwise paste the merchant's MLID
+ */
+function pasteMerchantInfo(arg) {
+  if (LaunchBar.options.alternateKey) {
+    LaunchBar.paste(`${arg.slug}`);
+  } else {
+    LaunchBar.paste(`${arg.mlid}`);
   }
+}
+
+function showHelp() {
+  LaunchBar.alert(
+    `Select a merchant then:
+  - Hit RETURN to paste the merchant's MLID in the current frontmost app
+  - Hit OPTION-RETURN to paste the merchant's slug instead`
+  )
+}
+
+/**
+ * Build the array of objects that'll be displayed as options by LaunchBar
+ */
+var actionItems = merchants.map((merchant) => {
+  var obj = {
+    title: merchant.name,
+    icon: 'womply-icon-64.png'
+  }
+  if (merchant.mlid) {
+    obj.subtitle = merchant.slug;
+    obj.alwaysShowSubtitle = true;
+    obj.badge = merchant.mlid;
+    obj.action = 'pasteMerchantInfo';
+    obj.actionArgument = merchant;
+    obj.actionRunsInBackground = true; // LaunchBar will crash if `LaunchBar.paste()` is not run in the background
+  }
+  return obj;
 })
 
-function run(argument) {
+actionItems.push({
+  title: 'Help!',
+  action: 'showHelp',
+  icon: 'font-awesome:question-circle'
+})
+
+/**
+ * The main function that'll execute when the action is run.
+ * Just gives you a list of merchants to choose from.
+ */
+function run() {
   return actionItems;
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+A little utility LaunchBar action to make my work a little bit easier and teach me a little bit more about LaunchBar action development.
